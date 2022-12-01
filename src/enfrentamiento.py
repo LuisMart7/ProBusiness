2cf80f5975c0029a3dd1c431e22024eea204d74b
Javier <fjgallardo00@gmail.com>
2022-12-01 15:29:39 +0100

    Refactor. Actualiza tipado de enfrentamiento

--- src/enfrentamiento.py
+++ src/enfrentamiento.py
@@ -1,6 +1,12 @@
+from dataclasses import dataclass
+
+@dataclass (frozen=True)
 class Enfrentamiento:
 
+    equipo1 :str
+    equipo2 :str
+
     # Se entiende por equipo1 como el equipo local y equipo2 como equipo visitante
-    def __init__(self, equipo1, equipo2):
+    def __init__(self, equipo1 :str, equipo2 :str):
         self.equipo1 = equipo1
         self.equipo2 = equipo2
