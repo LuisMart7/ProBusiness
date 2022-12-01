9ad9214c22f8c3e9298c4598837829095fcd7034
Javier <fjgallardo00@gmail.com>
2022-12-01 14:53:10 +0100

    Closes #15. Se elimina equipo.py

--- src/equipo.py
+++
@@ -1,9 +0,0 @@
-class Equipo:
-
-    # El atributo jugadores debe ser una lista
-    def __init__(self, nombre, jugadores):
-        self.nombre = nombre
-        self.jugadores = jugadores
-        
-    def añadir_jugador(self, jugador):
-        self.jugadores.append(jugador)
