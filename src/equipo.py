f9248ca0e0dd681c8363ca20d4db1d6593b6ab74
Javier <fjgallardo00@gmail.com>
2022-12-01 13:42:54 +0100

    Closes #15. Se define a equipo con nombre y una lista de jugadores

--- src/equipo.py
+++ src/equipo.py
@@ -1,32 +1,9 @@
 class Equipo:
 
-    def __init__(self, nombre):
+    # El atributo jugadores debe ser una lista
+    def __init__(self, nombre, jugadores):
         self.nombre = nombre
-        self.estadisticas_jugadores = []
-        self.partidos_jugados = 0
-        self.victorias = 0
-        self.empates = 0
-        self.derrotas = 0
-        self.puntos = 0
-        self.goles_a_favor = 0
-        self.goles_en_contra = 0
-        self.presupuesto_equipo = 0
-        self.posicion_liga = 1
-    
+        self.jugadores = jugadores
+        
     def añadir_jugador(self, jugador):
-        self.estadisticas_jugadores.append(jugador)
-
-    def get_victorias(self):
-        return self.victorias
-
-    def get_derrotas(self):
-        return self.derrotas
-
-    def get_empates(self):
-        return self.empates
-
-    def get_goles_a_favor(self):
-        return self.goles_a_favor
-
-    def get_goles_en_contra(self):
-        return self.goles_en_contra
+        self.jugadores.append(jugador)
