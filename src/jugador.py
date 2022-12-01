530dc1aa63e271aa26bd1239ecd8fc21585cd898
Javier <fjgallardo00@gmail.com>
2022-12-01 14:48:40 +0100

    Closes #16. Se elimina jugador.py

--- src/jugador.py
+++
@@ -1,6 +0,0 @@
-class Jugador:
-
-    # Se entiende posicion por una posicion genérica como delantero, mediocentro, defensa o portero.
-    def __init__(self, nombre, posicion):
-        self.nombre = nombre
-        self.posicion = posicion
