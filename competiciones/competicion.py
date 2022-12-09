4e1c212017d260ac7800cd961e5f78bba9f7633f
Javier <fjgallardo00@gmail.com>
2022-12-09 11:16:11 +0100

    Fix #19. Se arregla sintaxis y tipado de entidades
    Se arregla el tipado de las entidades para hacer buen uso de las dataclasses

--- competiciones/competicion.py
+++ competiciones/competicion.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
+from jornada import Jornada
 
 @dataclass
 class Competicion:
 
-    jornadas : list
+    jornadas : list(Jornada)
     clasificacion : dict
 
     def __init__(self):
         self.jornadas = []
-        self.clasificacion = []
+        self.clasificacion = {}
--- competiciones/jornada.py
+++ competiciones/jornada.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass
+from enfrentamiento import Enfrentamiento
 
 @dataclass
 class Jornada:
 
     # Debe de ser un pair que incluya la clase enfrentamiento y el resultado (1, X o 2)
-    enfrentamientos : list
+    enfrentamientos : list(Enfrentamiento)
 
     def __init__(self):
-        self.enfrentamiento = []
+        self.enfrentamientos = []
